9ecd502845544ae30fcf692c7d826d81098d3102
Ahmed Raza <ahmedraza449@gmail.com>
2021-07-14 12:45:02 +0500

    make it better

--- pointofsales/settings.py
+++ pointofsales/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(%(dp4=gx23y-(l_gia#kqy^w2b*t4)c7+-0=e@*igu1gy3z&4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','possystem1.herokuapp.com/']
 
 
 # Application definition
